6b7d10dbf27644f6a616dcafc4c5be5053e0b135
Jim Murphy <jim_murphy@shadywood.net>
2012-11-08 11:04:21 -0500

    Styles

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,18 +5,18 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
-      visit home_path
+      visit root_path
       page.should have_selector('h1', :text=> 'Sample App')
     end
 
     it "should have the right title" do
-      visit home_path
+      visit root_path
       page.should have_selector('title',
                 :text=> "Ruby on Rails Tutorial Sample App")
     end
 
     it "should not have the word Home in the title" do
-      visit home_path
+      visit root_path
       page.should_not have_selector('title',
                 :text=> "| Home")
     end
